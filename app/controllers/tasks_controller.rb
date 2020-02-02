832051b0bdd8a21028f34b6fad058a72ed0adc63
yusuke-hub <yk1210140408@gmail.com>
2020-02-02 01:38:00 +0000

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+  def edit
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
