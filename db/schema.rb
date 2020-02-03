53113fd9f167b2b83588e4305d0fc9c12ce13dbb
yusuke-hub <yk1210140408@gmail.com>
2020-02-03 23:31:00 +0000

    added admin flg to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_231213) do
+ActiveRecord::Schema.define(version: 2020_02_03_232926) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_231213) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
