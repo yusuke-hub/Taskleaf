fc02138456bc6ced47f3dec9142d89f173938819
yusuke-hub <yk1210140408@gmail.com>
2020-02-03 22:42:24 +0000

    added validation to tasks/name

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_235748) do
+ActiveRecord::Schema.define(version: 2020_02_03_223948) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
