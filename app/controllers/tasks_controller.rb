443c18e7aecd062c4d932731903ca69b9ea89199
yusuke-hub <yk1210140408@gmail.com>
2020-02-07 07:12:58 +0000

    completed implementation of Ajax task#index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,11 +31,11 @@ class TasksController < ApplicationController
   end
   def update
     @task.update(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
   def destroy 
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました"
     head :no_content
   end
   private
